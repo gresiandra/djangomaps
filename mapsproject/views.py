c741e26125d99bbced11e51df9305695de55f991
gresiandra <gresiandra@gmail.com>
2020-07-12 22:58:34 +0700

    api data to map SUCCESS

--- mapsproject/views.py
+++ mapsproject/views.py
@@ -5,23 +5,36 @@ import urllib.request as ur
 
 def index(request):
 
-    a = 1
+    a = 0
 
     response = ur.urlopen("https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=Kode_Provi,Provinsi,Kasus_Posi,Kasus_Semb,Kasus_Meni&outSR=4326&f=json")
     
     json_content = json.loads(response.read())
 
     while a <= 33:
-
-        print("Provinsi: " + str(json_content['features'][a]['attributes']['Provinsi']))
-        print("Positif: " + str(json_content['features'][a]['attributes']['Kasus_Posi']))
-        print("Sembuh: " + str(json_content['features'][a]['attributes']['Kasus_Semb']))
-        print("Meninggal: " + str(json_content['features'][a]['attributes']['Kasus_Meni']))
-        print('')
+        provinsi = json_content['features'][a]['attributes']['Provinsi']
+        kasus_positif = json_content['features'][a]['attributes']['Kasus_Posi']
+        kasus_sembuh = json_content['features'][a]['attributes']['Kasus_Semb']
+        kasus_meninggal = json_content['features'][a]['attributes']['Kasus_Meni']
         
+        if(a == 0):
+            aceh = []
+            aceh.append(provinsi)
+            aceh.append(kasus_positif)
+            aceh.append(kasus_sembuh)
+            aceh.append(kasus_meninggal)
+        
+        elif(a == 1):
+            sumut = []
+            sumut.append(provinsi)
+            sumut.append(kasus_positif)
+            sumut.append(kasus_sembuh)
+            sumut.append(kasus_meninggal)
+
         a=a+1
 
     context = {
-
+        'aceh':aceh,
+        'sumut':sumut,
     }
     return render(request, 'index.html', context)
